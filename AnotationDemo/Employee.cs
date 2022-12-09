2216ae07adff74f8f541c95dc1c4a8c428f1ef47
Darsh <darshankondhare2698@gmail.com>
2022-12-09 15:47:53 +0530

    [darsh] Add: Anotation Demo

---
+++ AnotationDemo/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotationDemo
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage = "Employee {0} is Required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minimun 3 Character and maxi 100 character")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(18, 99, ErrorMessage = "Age should be above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailID { get; set; }
+    }
+}
+
---
+++ AnotationDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AnotationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotation Problem ");
+            AnotationTest.Display();
+        }
+    }
+}
